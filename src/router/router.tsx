27733c0820677cd6b05b29f43728926c62b4f61e
Divyansh Garg <divyansh.garg@joshtechnologygroup.com>
2026-09-15 11:33:59 +0530

    [DG_A2_03]: Updated Login Page to use styled components

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router';
 
 import MainLayout from '@/layouts/MainLayout';
 import NotFound from '@/pages/NotFound/NotFound';
-import ReduxDemo from '@/pages/Demo/Demo';
 import Login from '@/pages/Login/Login';
 
 import { guestLoader } from '../utils/loaders';
@@ -11,10 +10,6 @@ export const router = createBrowserRouter([
     {
         element: <MainLayout />,
         children: [
-            {
-                path: '/',
-                element: <ReduxDemo />,
-            },
             {
                 path: '/login',
                 element: <Login />,
